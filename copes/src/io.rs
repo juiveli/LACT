72431403e8fb87631aa79e82b5fe7eee135ca8a4
juiveli <j00nas.makinen@hotmail.com>
2025-01-26 00:25:24 +0200

    changed copes repo

--- copes/src/io.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright 2022 Juan Palacios <jpalaciosdev@gmail.com>
-
-//! Input / Output utilities and related modules.
-
-pub mod connector;
-pub mod proc;
-
-mod socket;
--- copes/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright 2022 Juan Palacios <jpalaciosdev@gmail.com>
-
-#[allow(dead_code)]
-pub mod io;
-pub mod solver;
